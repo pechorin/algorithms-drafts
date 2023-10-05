9339d7bbc138e9cc9fe0a5a33f4638ad529fd504
pechorin andrey <pechorin.andrey@gmail.com>
2023-10-05 18:27:07 +0300

    selection sort

---
+++ sort/selection_sort.rb
@@ -0,0 +1,24 @@
+x = [13,5,43,2,3,3,2,4,10,6,1]
+
+x.size.times do |i|
+  min = x[i]
+  min_idx = i
+
+  n = i
+  while n != x.size
+    a = x[n]
+
+    if a < min
+      min = a
+      min_idx = n
+    end
+
+    n += 1
+  end
+
+  old = x[i]
+  x[i] = min
+  x[min_idx] = old
+end
+
+puts x.inspect
