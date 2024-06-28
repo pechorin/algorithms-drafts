19a9627ce7148813653a060b037d7d1b0d95450b
andrey pechorin <pechorin.andrey@gmail.com>
2024-06-28 10:09:11 +0300

    just another bubble morning training :) (don't ask me why)

---
+++ sort/bubble_sort_2.rb
@@ -0,0 +1,23 @@
+module Sort
+  class Bubble
+    def call(list)
+      0.upto(list.size - 1) do |n|
+        0.upto(list.size - n) do |n2|
+          next if list[n2 + 1].nil?
+
+          list[n2], list[n2 + 1] = list[n2 + 1], list[n2] if list[n2] > list[n2 + 1]
+        end
+      end
+
+      list
+    end
+  end
+end
+
+[
+  [3, 2, 1],
+  [10, 20, 5, 4, 2, 10, 12, 10],
+  [10, 1, 2, 3]
+].each do |list|
+  puts "Sort result for: #{list}\n#{Sort::Bubble.new.call(list)}"
+end
